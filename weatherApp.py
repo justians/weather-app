699de3cba91e989c9c975fb7ffab0705c1a9f472
justians <117990728+justians@users.noreply.github.com>
2023-10-29 16:39:03 -0400

    Update weatherApp.py

--- weatherApp.py
+++ weatherApp.py
@@ -1,14 +1,23 @@
 import requests
+import datetime
+import geocoder
+import time
 from bs4 import BeautifulSoup
 #url = 'https://www.youtube.com/watch?v=xjA1HjvmoMY'
 #url = 'https://www.wunderground.com/weather/us/md/baltimore'
-url = 'https://forecast.weather.gov/MapClick.php?lat=39.290580000000034&lon=-76.60925999999995'
+
+
+
+url = 'https://forecast.weather.gov/MapClick.php?lat=39.29&lon=-76.61'
+
 page = requests.get(url)
 
 soup = BeautifulSoup(page.text, 'html.parser') 
 
 tempature = soup.find('p', class_ = "myforecast-current-lrg").text
 
+
 message = f"The current tempature in Baltimore Maryland is {tempature}!"
+
 
 print(message)

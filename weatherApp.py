b10a65d052ab555eb063f72b54a86e9c75eecd46
justians <117990728+justians@users.noreply.github.com>
2023-12-15 10:30:58 -0500

    Update weatherApp.py

--- weatherApp.py
+++ weatherApp.py
@@ -3,32 +3,35 @@ import json
 import time
 from bs4 import BeautifulSoup
 
-target_location = input("Hello, enter target location for current tempature to be displayed: ")
+def location_finder(target_location):
 
-if(" " in target_location): #parsing/formatting the users input into a usable URL
-    target_location = target_location.replace(" ", "+")
+    if(" " in target_location): #parsing/formatting the users input into a usable URL
+        target_location = target_location.replace(" ", "+")
+    global api_url
+    api_url = f"https://geocode.maps.co/search?q={target_location}"
 
-api_url = f"https://geocode.maps.co/search?q={target_location}"
+#location_finder(input("Hello, enter target location for current tempature to be displayed: "))
 
-api_request = requests.get(api_url)
-api_data = json.loads(api_request.text)
-latitude = api_data[0]['lat'] #sifting through GEOCODE JSON file to find necessary attributes of the dict
-longitude = api_data[0]["lon"]
-location = api_data[0]["display_name"]
-#lat_and_lon = str(api_data[0]['lat']) + str(api_data[0]['lon'])
-#print(lat_and_lon, sep = ", ")
+def api_pulling():
+    api_request = requests.get(api_url)
+    api_data = json.loads(api_request.text)
+    latitude = api_data[0]['lat'] #sifting through GEOCODE JSON file to find necessary attributes of the dict
+    longitude = api_data[0]["lon"]
+    location = api_data[0]["display_name"]
+    #lat_and_lon = str(api_data[0]['lat']) + str(api_data[0]['lon'])
+    #print(lat_and_lon, sep = ", ")
+    url = f'https://forecast.weather.gov/MapClick.php?lat={latitude}&lon={longitude}'
 
+    page = requests.get(url)
 
-url = f'https://forecast.weather.gov/MapClick.php?lat={latitude}&lon={longitude}'
+    soup = BeautifulSoup(page.text, 'html.parser')#initial html parse
 
-page = requests.get(url)
+    tempature = soup.find('p', class_ = "myforecast-current-lrg").text #scrapes weather.gov's html for the current tempature
+    #location = soup.find('h2', class_ = "panel-title").text
+    print(tempature)
 
-soup = BeautifulSoup(page.text, 'html.parser')#initial html parse
+def main_loop():
+    location_finder(input("Hello, enter target location for current tempature to be displayed: "))
+    api_pulling()
 
-tempature = soup.find('p', class_ = "myforecast-current-lrg").text #scrapes weather.gov's html for the current tempature
-#location = soup.find('h2', class_ = "panel-title").text
-
-message = f"The current tempature in {location} is {tempature}!"
-
-
-print(message)
+main_loop()

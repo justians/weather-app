d1d9dea92b77bebd0ef8efe17423763d907169d7
justians <117990728+justians@users.noreply.github.com>
2023-12-19 10:30:41 -0500

    Update weatherApp.py

--- weatherApp.py
+++ weatherApp.py
@@ -1,29 +1,28 @@
 import requests
 import json
-import time
 from bs4 import BeautifulSoup
 
-weather_count = range(int(input("Number of locations you would like to find the weather for: ")))
-locations = {}
-for i in weather_count:
-    locations["location{0}".format(i)] = input(f"Enter location #{i}: ")
-    print(locations)
+location_count = int(input("Enter the amount of locations you want to find: "))
+locations = []
 
-"""def location_finder(target_location):
+gooyat = [locations.append(input(f"enter location #{i + 1}: ")) for i in range(location_count)]
+weather = [None for _ in range(location_count)]
+pairs = { k:v for (k,v) in zip(locations, weather)}  
+ 
+def main():
+    for place in pairs:
+        assign_temp(place)
 
-    if(" " in target_location): #parsing/formatting the users input into a usable URL
-        target_location = target_location.replace(" ", "+")
-    global api_url
-    api_url = f"https://geocode.maps.co/search?q={target_location}"
+def assign_temp(place):
+    if(" " in place): #parsing/formatting the users input into a usable URL
+        place = place.replace(" ", "+")
+    api_url = f"https://geocode.maps.co/search?q={place}"
 
-#location_finder(input("Hello, enter target location for current tempature to be displayed: "))
-
-def api_pulling():
     api_request = requests.get(api_url)
     api_data = json.loads(api_request.text)
     latitude = api_data[0]['lat'] #sifting through GEOCODE JSON file to find necessary attributes of the dict
     longitude = api_data[0]["lon"]
-    #location = api_data[0]["display_name"]
+    location = api_data[0]["display_name"]
     #lat_and_lon = str(api_data[0]['lat']) + str(api_data[0]['lon'])
     #print(lat_and_lon, sep = ", ")
     url = f'https://forecast.weather.gov/MapClick.php?lat={latitude}&lon={longitude}'
@@ -32,13 +31,8 @@ def api_pulling():
 
     soup = BeautifulSoup(page.text, 'html.parser')#initial html parse
 
-    tempature = soup.find('p', class_ = "myforecast-current-lrg").text #scrapes weather.gov's html for the current tempature
+    temperature = soup.find('p', class_ = "myforecast-current-lrg").text #scrapes weather.gov's html for the current tempature
     #location = soup.find('h2', class_ = "panel-title").text
-    print(tempature)
+    print(temperature)
 
-def main_loop():
-    for i in weather_count:
-        location_finder(input("Hello, enter target location for current tempature to be displayed: "))
-        api_pulling()
-
-main_loop()"""
+main()
